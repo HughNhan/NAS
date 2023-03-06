e3ae7f8864d281e893690dc16b93a15b4b98a31c
root <root@e16-h12-b01-fc640.rdu2.scalelab.redhat.com>
2023-03-06 14:36:51 +0000

    set mcp maxUnavailable to speedup

--- functions.sh
+++ functions.sh
@@ -128,6 +128,8 @@ Options:
     if [ ${WORKERS} == "none" ]; then 
         WORKERS=$(oc get node | grep worker | awk '{print $1}')
     fi
+    WORKER_LIST=( $WORKERS )
+    NUM_NAS_WORKERS=${#WORKER_LIST[@]}
 }
 
 
